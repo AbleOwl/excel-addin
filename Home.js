3b3a9451d635110217e8995e8eee909f432cb28a
AbleOwl <development@ableowl.com>
2017-09-29 12:33:12 +1300

    Update Home.js

--- Home.js
+++ Home.js
@@ -5,6 +5,7 @@
         $(document).ready(function () {
             $('#set-color').click(setColor);
               $('#open-dia').click(openBox);
+              $('#protect').click(protect);
         });
         
      function openBox(){
@@ -17,6 +18,22 @@
             var range = context.workbook.getSelectedRange();
             range.format.fill.color = 'green';
             alert('test');
+            return ctx.sync();
+        }).catch(function (error) {
+            console.log("Error: " + error);
+            if (error instanceof OfficeExtension.Error) {
+                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+            }
+        });
+    }
+    
+        function protect() {
+        Excel.run(function (context) {
+            var sheet = context.workbook.worksheets.getItem("Sheet1");
+          //  var range = context.workbook.getSelectedRange();
+          var range = sheet.getRange("A1:B3").format.protection.locked = false;
+           sheet.protection.protect({allowInsertRows:true});
+
             return ctx.sync();
         }).catch(function (error) {
             console.log("Error: " + error);
